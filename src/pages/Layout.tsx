9185d2d53442470f2a0ca55bb6a748f4720b9d6a
“Mona <monaelizabet@gmail.com>
2026-01-14 19:02:17 +0100

    fixing scroll

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -4,7 +4,7 @@ export const Layout = () => {
     return (
         <div className="relative">
             {/* Large gradient circle */}
-            <div className="absolute -top-50 -right-40  w-1/2 h-140 rounded-full bg-linear-to-br from-primary/20 via-accent/10 to-transparent blur-3xl" />
+            <div className="absolute right-0  w-1/2 h-140 rounded-full bg-linear-to-br from-primary/20 via-accent/10 to-transparent blur-3xl" />
 
             <div className="">
                 <main className="p-5">
